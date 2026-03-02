37c96e3d4781c2de772683fff91ca59729d6f285
HQIM <crystal.rentz@safalpartners.com>
2026-03-02 16:45:29 -0600

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,4 @@
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40">
+  <rect width="40" height="40" rx="8" fill="#1B4965"/>
+  <text x="50%" y="55%" text-anchor="middle" dominant-baseline="middle" font-family="Georgia, serif" font-size="24" font-weight="bold" fill="#C5A768">S</text>
+</svg>
